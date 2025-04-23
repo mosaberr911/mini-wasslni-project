71ca773bec3d8484137e039f57f43815227f626d
kyrollos10520 <kyrolloskaram504@gmail.com>
2025-04-23 07:41:01 +0200

    created by kyrollos and Ashraf

---
+++ task/Graph.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+using namespace std;
+
+class Graph
+{
+	map<string, vector<pair<string, int>>> graph;
+public:
+	public:
+	Graph();
+	void Addgraph();
+	void displaygraph();
+};
+
