3a1f611e7dc9c3375dd18b0058652b718a3a78af
mosaberr911 <mohamed9.saber6@gmail.com>
2025-05-03 20:59:08 +0300

    Update options.h

--- wasslni/options.h
+++ wasslni/options.h
@@ -15,14 +15,20 @@ public:
 private slots:
     void onShowShortestPathClicked();
     void onShowPathClicked();
+    void onDisplayMapClicked(); // إضافة التابع الخاص بـ Display Map
+    void onAddCityClicked(); // إضافة التابع الخاص بـ Add City
 
 private:
     QPushButton *showShortestPathButton;
     QPushButton *showPathButton;
+    QPushButton *displayMapButton; // إضافة زر Display Map
+    QPushButton *addCityButton; // إضافة زر Add City
     QLineEdit *startCityLineEdit;
     QLineEdit *endCityLineEdit;
+    QLineEdit *addCityLineEdit; // إضافة QLineEdit لمدينة جديدة
 
-    bool isPathInputVisible = false; // <-- مضاف لتتبع حالة الإظهار
+    bool isPathInputVisible = false;
+    bool isAddCityInputVisible = false; // إضافة متغير لتتبع حالة إظهار صندوق نص المدينة
 
     QVector<std::tuple<QString, QString, int>> loadEdgesFromFile(const QString& filePath);
 };
