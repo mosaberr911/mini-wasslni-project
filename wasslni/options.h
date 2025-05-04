8e5ed06302c8af71815417fa3cf1653feb3bc301
mosaberr911 <mohamed9.saber6@gmail.com>
2025-05-04 13:04:41 +0300

    Update options.h

--- wasslni/options.h
+++ wasslni/options.h
@@ -20,6 +20,8 @@ private slots:
     void onDisplayMapClicked(); // إضافة التابع الخاص بـ Display Map
     void onAddCityClicked(); // إضافة التابع الخاص بـ Add City
     void onSaveCityClicked(); // إضافة التابع الخاص بـ Save City
+    void onAddRoadClicked(); // إضافة التابع الخاص بـ Add Road
+    void onSaveRoadClicked(); // إضافة التابع الخاص بـ Save Road
 
 private:
     QPushButton *showShortestPathButton;
@@ -27,14 +29,23 @@ private:
     QPushButton *displayMapButton; // إضافة زر Display Map
     QPushButton *addCityButton; // إضافة زر Add City
     QPushButton *saveCityButton; // إضافة زر Save City
+    QPushButton *addRoadButton; // إضافة زر Add Road
+    QPushButton *saveRoadButton; // إضافة زر Save Road
+
     QLineEdit *startCityLineEdit;
     QLineEdit *endCityLineEdit;
     QLineEdit *addCityLineEdit; // إضافة QLineEdit لمدينة جديدة
+    QLineEdit *roadStartCityLineEdit; // إضافة QLineEdit لبداية الطريق
+    QLineEdit *roadEndCityLineEdit; // إضافة QLineEdit لنهاية الطريق
+    QLineEdit *roadDistanceLineEdit; // إضافة QLineEdit للمسافة
 
     bool isPathInputVisible = false;
     bool isAddCityInputVisible = false; // إضافة متغير لتتبع حالة إظهار صندوق نص المدينة
+    bool isAddRoadInputVisible = false; // إضافة متغير لتتبع حالة إظهار حقول الطريق
 
     QVector<std::tuple<QString, QString, int>> loadEdgesFromFile(const QString& filePath);
+
+    void saveRoadToFile(const QString& city1, const QString& city2, int distance);
     void saveCityToFile(const QString& cityName); // دالة لحفظ المدينة في الملف
 };
 
