eb057360f005939814f5c04f74dcd5072b8ba152
mosaberr911 <mohamed9.saber6@gmail.com>
2025-05-04 07:18:17 +0300

    Update options.h

--- wasslni/options.h
+++ wasslni/options.h
@@ -4,6 +4,8 @@
 #include <QWidget>
 #include <QPushButton>
 #include <QLineEdit>
+#include <QVector>
+#include <tuple>
 
 class Options : public QWidget
 {
@@ -17,12 +19,14 @@ private slots:
     void onShowPathClicked();
     void onDisplayMapClicked(); // إضافة التابع الخاص بـ Display Map
     void onAddCityClicked(); // إضافة التابع الخاص بـ Add City
+    void onSaveCityClicked(); // إضافة التابع الخاص بـ Save City
 
 private:
     QPushButton *showShortestPathButton;
     QPushButton *showPathButton;
     QPushButton *displayMapButton; // إضافة زر Display Map
     QPushButton *addCityButton; // إضافة زر Add City
+    QPushButton *saveCityButton; // إضافة زر Save City
     QLineEdit *startCityLineEdit;
     QLineEdit *endCityLineEdit;
     QLineEdit *addCityLineEdit; // إضافة QLineEdit لمدينة جديدة
@@ -31,6 +35,7 @@ private:
     bool isAddCityInputVisible = false; // إضافة متغير لتتبع حالة إظهار صندوق نص المدينة
 
     QVector<std::tuple<QString, QString, int>> loadEdgesFromFile(const QString& filePath);
+    void saveCityToFile(const QString& cityName); // دالة لحفظ المدينة في الملف
 };
 
 #endif // OPTIONS_H
