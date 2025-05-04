5e6e429bf8e8ba9fa8c938eb568d0e1f37d0b581
mosaberr911 <mohamed9.saber6@gmail.com>
2025-05-04 07:18:52 +0300

    Update graph.h

--- wasslni/graph.h
+++ wasslni/graph.h
@@ -15,14 +15,18 @@ class Graph
 {
 public:
     void addGraphFromUI(const QVector<std::tuple<QString, QString, int>>& edges);
-    std::string dijkstra(const std::string& start, const std::string& end);  // دالة Dijkstra
-    std::string displayMap();  // تم إضافة دالة displayMap
+    std::string dijkstra(const std::string& start, const std::string& end);
+    std::string displayMap();
+
+    void addCity(std::string cityName);          // دالة لإضافة مدينة
+    void deleteCity(std::string cityName);       // دالة لحذف مدينة
+    bool containsCity(std::string cityName);     // دالة لفحص وجود المدينة
 
 private:
-    std::map<std::string, std::vector<std::pair<std::string, int>>> adj;  // قائمة الجوار
-    std::map<std::string, double> dis;  // لتخزين المسافات
-    std::map<std::string, bool> vis;  // لتخزين إذا تم زيارة العقدة
-    std::map<std::string, std::string> parent;  // لتخزين الأب لكل عقدة
+    std::map<std::string, std::vector<std::pair<std::string, int>>> adj;
+    std::map<std::string, double> dis;
+    std::map<std::string, bool> vis;
+    std::map<std::string, std::string> parent;
 };
 
 #endif // GRAPH_H
