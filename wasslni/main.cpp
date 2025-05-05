7a3cbd697ce2403291824d4c13e3e69e514d8c92
mosaberr911 <mohamed9.saber6@gmail.com>
2025-05-05 21:16:45 +0300

    Update main.cpp

--- wasslni/main.cpp
+++ wasslni/main.cpp
@@ -1,11 +1,16 @@
 #include "login.h"
-
 #include <QApplication>
+#include <QIcon>  
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    a.setWindowIcon(QIcon(":/images/favicon.ico"));
+
     Login w;
+    w.setWindowIcon(QIcon(":/images/favicon.ico"));  
     w.show();
+
     return a.exec();
 }
