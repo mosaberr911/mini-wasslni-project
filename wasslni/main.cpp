dffbdc418be188e8d65d84774006c052a2250519
mosaberr911 <mohamed9.saber6@gmail.com>
2025-05-07 01:09:08 +0300

    Update main.cpp

--- wasslni/main.cpp
+++ wasslni/main.cpp
@@ -1,15 +1,16 @@
 #include "login.h"
 #include <QApplication>
-#include <QIcon>  
+#include <QIcon>  // تأكد من إضافة هذه المكتبة
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    // تعيين أيقونة التطبيق الرئيسية
     a.setWindowIcon(QIcon(":/images/favicon.ico"));
 
     Login w;
-    w.setWindowIcon(QIcon(":/images/favicon.ico"));  
+    w.setWindowIcon(QIcon(":/images/favicon.ico"));  // تعيين الأيقونة للنافذة
     w.show();
 
     return a.exec();
