80b297ee01767d61040fdc285d06a0a1af97e12e
mosaberr911 <mohamed9.saber6@gmail.com>
2025-05-08 18:52:23 +0300

    Update Graph.h

--- wasslni/Graph.h
+++ wasslni/Graph.h
@@ -1,14 +1,14 @@
 #ifndef GRAPH_H
 #define GRAPH_H
-
+#include <queue>
 #include <QString>
 #include <QVector>
 #include <tuple>
 #include <map>
 #include <vector>
 #include <string>
-#include <queue>
 #include <functional>
+#include <QTextEdit>
 #include <limits>
 #include <unordered_set>
 #include <fstream>
@@ -18,15 +18,16 @@
 class Graph
 {
 public:
+    Graph(const std::string& userEmail = "");  // Constructor with optional userEmail
 
-    Graph(const std::string& userEmail = "");  // Updated constructor
-
-    void setUserEmail(const std::string& email);  // New method
-    std::string getUserGraphPath() const;  // New method
+    void setUserEmail(const std::string& email);  // Setter for userEmail
+    std::string getUserGraphPath() const;  // Getter for user graph file path
 
     void addGraphFromUI(const QVector<std::tuple<QString, QString, int>>& edges);
     std::string dijkstra(const std::string& start, const std::string& end);
     std::string displayMap();
+    void BFS(const QString& startNode, QTextEdit* output);
+    void DFS(const QString& startNode, QTextEdit* output);
 
     bool addCity(std::string cityName);
     bool addEdge(std::string city1, std::string city2, float distance);
@@ -35,7 +36,7 @@ public:
     bool deleteCity(std::string cityName);
 
 private:
-    std::string userEmail;  // New member variable
+    std::string userEmail;  // User email for file path creation
     std::map<std::string, std::vector<std::pair<std::string, float>>> adj;
     std::map<std::string, double> dis;
     std::map<std::string, bool> vis;
