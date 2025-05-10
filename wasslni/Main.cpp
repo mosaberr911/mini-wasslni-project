8e4ee25fedcb3ab9443ae5323e8b2d9074c31249
Mohamed Yassin Elsafty <myassinelsafty@gmail.com>
2025-05-10 23:30:46 +0300

    Update Main.cpp | Implement New Files Logic

--- wasslni/Main.cpp
+++ wasslni/Main.cpp
@@ -1,9 +1,27 @@
-#include "login.h"
+#include "Login.h"
+#include "FileManager.h"
+#include "PathManager.h"
+#include "UserManager.h"
 #include <QApplication>
-#include <QIcon>  
+#include <QIcon>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+    // SETTING PATH
+    PathManager::setSystemPath("/Users/mohamed/CLionProjects/mini-wasslni-project/wasslni/");
+
+    // CONSTANT FILE/IMAGE NAMES
+    PathManager::setBackgroundFolderPath("images/Screenshot 2025-04-28 183435.png");
+    PathManager::setUsersFilename("users.txt");
+    PathManager::setGraphsFilename("graphs.txt");
+
+    try {
+        UserManager::loadUsersData();
+    } catch (const exception& e) {
+        qWarning() << "ERROR: Failed to read users data: " << e.what();
+    }
+
     QApplication a(argc, argv);
 
     a.setWindowIcon(QIcon(":/images/favicon.ico"));
@@ -12,5 +30,7 @@ int main(int argc, char *argv[])
     w.setWindowIcon(QIcon(":/images/favicon.ico"));  
     w.show();
 
-    return a.exec();
+    int result = a.exec();
+    UserManager::saveUsersData();
+    return result;
 }
