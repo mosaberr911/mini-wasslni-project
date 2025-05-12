8162686691cc4db4d3db597a965d1be82cbc0b22
Mohamed Yassin Elsafty <myassinelsafty@gmail.com>
2025-05-12 22:51:49 +0300

    Update Login.h | added log out signals

--- wasslni/Login.h
+++ wasslni/Login.h
@@ -5,6 +5,7 @@
 #include "Register.h"
 #include "Map.h"
 #include "Options.h"
+#include <string>
 #include <QString>
 #include <QMessageBox>
 #include <QDir>  
@@ -18,14 +19,19 @@ class Login : public QWidget
 
 public:
     explicit Login(QWidget *parent = nullptr);
+    virtual ~Login();
 
 private slots:
     void onCreateAccountClicked();
     void showLogin();
     void onLoginClicked();
+    void onUserLoggedOut();
+    void onMapLoggedOut();
+    void onAddNewMap();
+    void showMapWindow();
 
 private:
-
+    string userEmail;
     QLineEdit *emailField;
     QLineEdit *passwordField;
     QLabel *createAccountButton;
