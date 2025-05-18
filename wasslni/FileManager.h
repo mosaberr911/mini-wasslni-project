d2f3f8bfe7dc40112141ed69324a5e2e9334b2d6
Mohamed Yassin Elsafty <myassinelsafty@gmail.com>
2025-05-18 17:04:51 +0300

    Update FileManager.h | unordered_map, remove unused functions

--- wasslni/FileManager.h
+++ wasslni/FileManager.h
@@ -7,7 +7,7 @@
 #include "User.h"
 
 using namespace std;
-using AdjacencyList = map<string, vector<pair<string, float> > >;
+using AdjacencyList = unordered_map<string, vector<pair<string, float> > >;
 
 class FileManager
 {
@@ -18,9 +18,6 @@ public:
     static void deleteUserGraph(const string& filename, User& targetUser);
     static void saveUsers(const string& filename, const unordered_map<string, User>& users);
     static unordered_map<string, User> loadUsers(const string& filename);
-    static string getCurrentPath();
-    static void setCurrentPath(const string&);
-    static string getFullPath(const string&);
     static bool fileExists(const string& path);
 };
 
